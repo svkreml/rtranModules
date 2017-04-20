b1f70b96979bb945692e931fdf83fe8fc69d7e13
Андрей <viruszold@mail.ru>
2017-04-20 17:11:15 +0300

    Формирование единого JAR файла. После сборк в папке interface/target/single.jar будет находится сам файл, а в директории interface находтся скрипт запуска start.bat

---
+++ interface/start.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+SetLocal EnableDelayedExpansion
+set JAVA=C:\jdk1.8.0_91\jre\bin\java.exe
+rem set DUMP=-Dcom.sun.xml.ws.transport.http.HttpAdapter.dump=true
+rem -Dlogback.configurationFile=/path/to/config.xml
+
+%JAVA% -jar target/single.jar
